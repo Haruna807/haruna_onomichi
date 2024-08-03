225e08fb95430cc3f79dc6b310c56cb600688316
haruna <k.haruna1145@gmail.com>
2024-08-04 00:14:16 +0900

    fvレスポンシブ、aboutセクション作成途中

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-document.querySelector("#js-drawer-button").addEventListener("click", function(e) {
-  e.preventDefault();
+jQuery("#js-drawer-button").on("click", function(e) {
+    e.preventDefault();
+    jQuery("#js-drawer-button").toggleClass("is-checked");
 
-  // #js-drawer-button と #js-drawer-content に .is-checked クラスをトグル
-  document.querySelector("#js-drawer-button").classList.toggle("is-checked");
-  document.querySelector("#js-drawer-content").classList.toggle("is-checked");
+    jQuery("#js-drawer-content").slideToggle(280);
+});
 
-  // 画像の src を切り替える
-  var img = document.querySelector("#js-drawer-button img");
-  if (document.querySelector("#js-drawer-button").classList.contains("is-checked")) {
-      img.src = "./img/sp/drawer-icon-close.png"; // .is-checked のときの画像を設定
-      img.style.width = "40px"; // 拡大したいサイズに設定
-  } else {
-      img.src = "./img/sp/drawer-icon.png"; // 初期の画像を設定
-      img.style.width = ""; // 初期サイズに戻す場合は空文字列にするか、元のサイズを設定する
-  }
+
+$(document).ready(function() {
+  $("#js-drawer-button").click(function() {
+    var img = $("#js-drawer-button img");
+
+    if ($(this).hasClass("is-checked")) {
+      img.attr("src", "./img/sp/drawer-icon-close.png"); // .is-checked のときの画像を設定
+      img.css("width", "40px"); // 拡大したいサイズに設定
+    } else {
+      img.attr("src", "./img/sp/drawer-icon.png"); // 初期の画像を設定
+      img.css("width", ""); // 初期サイズに戻す場合は空文字列にするか、元のサイズを設定する
+    }
+  });
 });
