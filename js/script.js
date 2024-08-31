af5fa70093deb1c23c6623545d5d39d95f3080e1
haruna <k.haruna1145@gmail.com>
2024-08-31 22:44:33 +0900

    スムーススクロール、ホバーの実装

--- js/script.js
+++ js/script.js
@@ -1,24 +1,78 @@
-jQuery("#js-drawer-button").on("click", function(e) {
-    e.preventDefault();
-    jQuery("#js-drawer-button").toggleClass("is-checked");
+// jQuery("#js-drawer-button").on("click", function(e) {
+//     e.preventDefault();
+//     jQuery("#js-drawer-button").toggleClass("is-checked");
 
-    jQuery("#js-drawer-content").slideToggle(280);
-});
+//     jQuery("#js-drawer-content").slideToggle(280);
+// });
+
+// // ドロワーメニューのリンクが押された時、メニューが閉じる
+
+// jQuery("#js-drawer-content a[href^='#']").on("click", function(e) {
+//   e.preventDefault();
+
+//   var target = jQuery(this.getAttribute("href"));
+//   var offsetTop = target.offset().top;
+
+//   jQuery("#js-drawer-content").slideUp(280, function() {
+//     jQuery("#js-drawer-button").removeClass("is-checked");
+//   });
+// });
+
+
+
+// $(document).ready(function() {
+//   $("#js-drawer-button").click(function() {
+//     var img = $("#js-drawer-button img");
+
+//     if ($(this).hasClass("is-checked")) {
+//       img.attr("src", "./img/sp/drawer-icon-close.png"); // .is-checked のときの画像を設定
+//       img.css("width", "40px"); // 拡大したいサイズに設定
+//     } else {
+//       img.attr("src", "./img/sp/drawer-icon.png"); // 初期の画像を設定
+//       img.css("width", ""); // 初期サイズに戻す場合は空文字列にするか、元のサイズを設定する
+//     }
+//   });
+// });
 
 
 $(document).ready(function() {
-  $("#js-drawer-button").click(function() {
-    var img = $("#js-drawer-button img");
-
+  $("#js-drawer-button").on("click", function(e) {
+    e.preventDefault();
+    $(this).toggleClass("is-checked");
+    
+    var img = $(this).find("img");
     if ($(this).hasClass("is-checked")) {
-      img.attr("src", "./img/sp/drawer-icon-close.png"); // .is-checked のときの画像を設定
-      img.css("width", "40px"); // 拡大したいサイズに設定
+      img.attr("src", "./img/sp/drawer-icon-close.png");
+      img.css("width", "40px");
     } else {
-      img.attr("src", "./img/sp/drawer-icon.png"); // 初期の画像を設定
-      img.css("width", ""); // 初期サイズに戻す場合は空文字列にするか、元のサイズを設定する
+      img.attr("src", "./img/sp/drawer-icon.png");
+      img.css("width", "");
     }
+    
+    $("#js-drawer-content").slideToggle(250);
+  });
+
+  $("#js-drawer-content a[href^='#']").on("click", function(e) {
+    e.preventDefault();
+    
+    var target = $($(this).attr("href"));
+    var offsetTop = target.offset().top;
+    
+    $("html, body").animate({ scrollTop: offsetTop }, 0, function() {
+      $("#js-drawer-content").slideUp(250, function() {
+        $("#js-drawer-button").removeClass("is-checked");
+        var img = $("#js-drawer-button img");
+        img.attr("src", "./img/sp/drawer-icon.png");
+        img.css("width", "");
+      });
+    });
+    
+    $("html, body").animate({ scrollTop: offsetTop }, 0);
   });
 });
+
+
+
 
 // スワイパー
 
@@ -206,7 +260,7 @@ $(function () {
   pagetop.click(function () {
     $("body,html").animate({
       scrollTop: 0,
-    },500);
+    },0);
     return false;
   });
 });
